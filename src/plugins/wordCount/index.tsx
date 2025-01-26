d1f4cc631b60da2dc3579cdb6e778a5e1c8e4c88
lumap <lumap@duck.com>
2025-01-26 20:23:50 +0100

    fix wordcount

--- src/plugins/wordCount/index.tsx
+++ src/plugins/wordCount/index.tsx
@@ -4,13 +4,21 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { addAccessory } from "@api/MessageAccessories";
+import { addMessageAccessory } from "@api/MessageAccessories";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 import { Text } from "@webpack/common";
 
-function WordCount({ messageContent, authorId }: { messageContent: string, authorId: string; }) {
-    const words = messageContent.split(/\s+/).filter((word: string) => word.length > 0);
+function WordCount({
+    messageContent,
+    authorId,
+}: {
+    messageContent: string;
+    authorId: string;
+}) {
+    const words = messageContent
+        .split(/\s+/)
+        .filter((word: string) => word.length > 0);
     if (authorId !== "570524986109067265") return null;
     if (words.length < 2) return null;
 
@@ -32,8 +40,15 @@ export default definePlugin({
     authors: [Devs.Lumap],
     dependencies: ["MessageAccessoriesAPI"],
     async start() {
-        addAccessory("word-count", (props: Record<string, any>) => (
-            <WordCount messageContent={props.message.content} authorId={props.message.author.id} />
-        ), 2);
-    }
+        addMessageAccessory(
+            "word-count",
+            (props: Record<string, any>) => (
+                <WordCount
+                    messageContent={props.message.content}
+                    authorId={props.message.author.id}
+                />
+            ),
+            2,
+        );
+    },
 });
