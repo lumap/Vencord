38f01498f8ff56ea9a9ff724f7810500609c0310
Delphox <smugleaf@live.com>
2026-04-18 03:40:49 -0300

    fix imports
    i wasn't at the latest vencord and this broke 2 days ago lmao

--- src/plugins/addAttachments/index.tsx
+++ src/plugins/addAttachments/index.tsx
@@ -8,7 +8,8 @@ import { UploadIcon } from "@components/Icons";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 import { ChannelStore, MessageStore, PermissionsBits, PermissionStore, SelectedChannelStore, showToast, Toasts, UserStore } from "@webpack/common";
-import { Common, findByPropsLazy } from "webpack";
+import { findByPropsLazy } from "@webpack";
+import * as Common from "@webpack/common";
 
 const uniqueIdProp = findByPropsLazy("uniqueId");
 
